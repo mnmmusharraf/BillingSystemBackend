30841306cccf27a9d9338eba5235394ff150ce4d
mnm musharraf <mnmmusharraf@gmail.com>
2025-08-01 11:58:13 +0530

    configured filters

--- src/main/java/com/pahana/billingsystembackend/filters/CORSFilter.java
+++ src/main/java/com/pahana/billingsystembackend/filters/CORSFilter.java
@@ -1,13 +1,24 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.pahana.billingsystembackend.filters;
 
-/**
- *
- * @author musharraf
- */
-public class CORSFilter {
+import jakarta.ws.rs.container.ContainerRequestContext;
+import jakarta.ws.rs.container.ContainerResponseContext;
+import jakarta.ws.rs.container.ContainerResponseFilter;
+import jakarta.ws.rs.ext.Provider;
+import java.io.IOException;
+
+@Provider
+public class CORSFilter implements ContainerResponseFilter{
+
+    @Override
+    public void filter(ContainerRequestContext requestContext, 
+                       ContainerResponseContext responseContext) throws IOException {
+        responseContext.getHeaders().add("Access-Control-Allow-Origin","*");
+        responseContext.getHeaders().add("Access-Control-Allow-Headers",
+                "origin, content-type, accept, authorization");
+        responseContext.getHeaders().add("Access-Control-Allow-Methods",
+                "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+    }
+    
+    
     
 }
