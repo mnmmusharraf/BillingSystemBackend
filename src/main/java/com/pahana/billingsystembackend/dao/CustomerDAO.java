7d494c6b5f9bd050bf11909a15a4223c40f258d9
mnm musharraf <mnmmusharraf@gmail.com>
2025-08-03 13:20:01 +0530

    getAllCustomer methos implementd

--- src/main/java/com/pahana/billingsystembackend/dao/CustomerDAO.java
+++ src/main/java/com/pahana/billingsystembackend/dao/CustomerDAO.java
@@ -1,13 +1,86 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.pahana.billingsystembackend.dao;
 
-/**
- *
- * @author musharraf
- */
+import com.pahana.billingsystembackend.db.DBConnection;
+import com.pahana.billingsystembackend.model.Customer;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLDataException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class CustomerDAO {
-    
+
+    private String generateNextAccountNumber() {
+        String prefix = "EDU-CUS-";
+        String query = "SELECT account_number FROM customer ORDER BY account_number DESC LIMIT 1";
+
+        try (Connection conn = DBConnection.getConnection(); PreparedStatement stmt = conn.prepareStatement(query)) {
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()) {
+                String lastAccountNumber = rs.getString("account_number");
+                int numberPart = Integer.parseInt(lastAccountNumber.substring(prefix.length()));
+                int nextNumber = numberPart + 1;
+                return String.format("%s%02d", prefix, nextNumber);
+            } else {
+                return "EDU-CUS-01";
+            }
+        } catch (SQLException | NumberFormatException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    // Method to add Customer
+    public boolean addCustomer(Customer customer) {
+        String newAccountNumber = generateNextAccountNumber();
+        if (newAccountNumber == null) {
+            return false;
+        }
+
+        String query = "INSERT INTO customer (account_number, name, address, telephone, units_consumed) VALUES (?,?,?,?,?)";
+
+        try (Connection conn = DBConnection.getConnection(); PreparedStatement stmt = conn.prepareStatement(query)) {
+            stmt.setString(1, newAccountNumber);
+            stmt.setString(2, customer.getName());
+            stmt.setString(3, customer.getAddress());
+            stmt.setString(4, customer.getTelephone());
+            stmt.setInt(5, customer.getConsumedUnits());
+
+            int rows = stmt.executeUpdate();
+
+            return rows > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+
+    }
+
+    // Mehtod to get All Customers from the db
+    public List<Customer> getAllCustomer(){
+        List<Customer> customerList = new ArrayList<>();
+        
+        String query = "SELECT * FORM customer";
+        
+        try(Connection conn = DBConnection.getConnection();
+                PreparedStatement stmt = conn.prepareStatement(query);
+                ResultSet rs = stmt.executeQuery()){
+            while(rs.next()){
+                Customer customer = new Customer();
+                customer.setAccountNumber(rs.getString("account_number"));
+                customer.setName(rs.getString("name"));
+                customer.setAddress(rs.getString("address"));
+                customer.setTelephone(rs.getString("telephone"));
+                customer.setConsumedUnits(rs.getInt("units_consumed"));
+                
+                customerList.add(customer);
+            }
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+         return customerList;
+    }
 }
